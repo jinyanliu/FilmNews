23b6bb09711a03e13e6efd983c7b1cb3b9d940ec
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-12-06 13:35:14 +0100

    Modify comments in FilmNewsAdapter to pursue a better code quality.

--- app/src/main/java/se/sugarest/jane/filmnews/FilmNewsAdapter.java
+++ app/src/main/java/se/sugarest/jane/filmnews/FilmNewsAdapter.java
@@ -20,6 +20,10 @@ import static se.sugarest.jane.filmnews.MainActivity.LOG_TAG;
  * Created by jane on 12/5/16.
  */
 
+/**
+ * FilmNewsAdapter is an ArrayAdapter that can provide the layout for the list
+ * based on a data source, whish is a list of FilmNews objects.
+ */
 public class FilmNewsAdapter extends ArrayAdapter<FilmNews> {
 
     private static final String DATE_SEPARATOR = "T";
@@ -32,13 +36,21 @@ public class FilmNewsAdapter extends ArrayAdapter<FilmNews> {
      * This is my own custom constructor(it doesn't mirror a superclass constructor).
      * The context is used to inflate the layout file, and the list is the data I want to populate into the list.
      *
-     * @param context The current context. Used to inflate the layout file.
+     * @param context  The current context. Used to inflate the layout file.
      * @param filmNews A list of FilmNews objects to display in a list.
      */
-    public FilmNewsAdapter(Activity context, ArrayList<FilmNews> filmNews){
+    public FilmNewsAdapter(Activity context, ArrayList<FilmNews> filmNews) {
         super(context, 0, filmNews);
     }
 
+    /**
+     * Provides a view for an AdapterView(ListView, GridView, etc.)
+     *
+     * @param position    The position in the list of data that should be displayed in the list item view.
+     * @param convertView The recycled view to populate.
+     * @param parent      The parent ViewGroup that is used for inflation.
+     * @return The View for the position in the AdapterView.
+     */
     @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
@@ -52,34 +64,34 @@ public class FilmNewsAdapter extends ArrayAdapter<FilmNews> {
         FilmNews currentFilmNews = getItem(position);
 
         ViewHolder holder = new ViewHolder();
-        holder.articleTitleTextView = (TextView)convertView.findViewById(R.id.article_title);
+        //Find the TextView in the list_item.xml layout with the ID article_title
+        holder.articleTitleTextView = (TextView) convertView.findViewById(R.id.article_title);
+        //Display the title of the current article in that TextView.
         holder.articleTitleTextView.setText(currentFilmNews.getArticleTitle());
-        holder.sectionNameTextView = (TextView)convertView.findViewById(R.id.section_name);
+        //Find the TextView in the list_item.xml layout with the ID section_name
+        holder.sectionNameTextView = (TextView) convertView.findViewById(R.id.section_name);
+        //Display the section name of the current article in that TextView.
         holder.sectionNameTextView.setText(currentFilmNews.getSectionName());
 
+        //Split the publish date string of the current article from DATE_SEPARATOR
         String[] parts = currentFilmNews.getPublishDate().split(DATE_SEPARATOR);
+        //Get only the first part, date information.
         publishDatePartOne = parts[0];
 
+        //Format date format
         DateFormat inputDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         DateFormat outputDateFormat = new SimpleDateFormat("MMM d, yyyy");
-
         String inputDateString = publishDatePartOne;
-
-//        Date date = inputDateFormat.parse(inputDateString);
-//        String outputDateString = outputDateFormat.format(date);
-
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy");
-
         try {
-//            Date publishDate = dateFormat.parse(publishDatePartOne);
-//            formattedDate = formatDate(publishDate);
             Date date = inputDateFormat.parse(inputDateString);
             formattedDate = outputDateFormat.format(date);
         } catch (java.text.ParseException e) {
             Log.e(LOG_TAG, "Problem formatting Date Object.", e);
         }
 
-        holder.publishDateTextView = (TextView)convertView.findViewById(R.id.publish_date);
+        //Find the TextView in the list_item.xml layout with the ID publish_date
+        holder.publishDateTextView = (TextView) convertView.findViewById(R.id.publish_date);
+        //Display the formatted publish date of the current article in that TextView. (i.e. "Mar 3, 2016")
         holder.publishDateTextView.setText(formattedDate);
 
         convertView.setTag(holder);
@@ -87,13 +99,5 @@ public class FilmNewsAdapter extends ArrayAdapter<FilmNews> {
         //Return the list_item view layout (containing 3 TextViews)
         //so that it can be shown in the ListView
         return convertView;
-    }
-
-    /**
-     * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
-     */
-    private String formatDate(Date dateObject) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy");
-        return dateFormat.format(dateObject);
     }
 }
