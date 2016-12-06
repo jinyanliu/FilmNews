f7281c85b3126e3e47a23a39e04d6d36225fbcb0
JinyanLiu <jane.jinyan.liu@gmail.com>
2016-12-06 13:47:34 +0100

    Modify comments in MainActivity java file to pursue a better code quality.

--- app/src/main/java/se/sugarest/jane/filmnews/MainActivity.java
+++ app/src/main/java/se/sugarest/jane/filmnews/MainActivity.java
@@ -10,7 +10,6 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -48,7 +47,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     @Override
     public Loader<List<FilmNews>> onCreateLoader(int id, Bundle args) {
         //Create a new loader for the given URL
-        Log.i(LOG_TAG, "onCreateLoader");
         return new FilmNewsLoader(this, FILMNEWS_REQUEST_URL);
     }
 
@@ -60,11 +58,9 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
 
         // Clear the adapter of previous filmNews data
         mAdapter.clear();
-        Log.i(LOG_TAG, "onLoadFinished");
 
         //If there is a network connection, fetch data
         if (getNetworkInfo() != null && getNetworkInfo().isConnected()) {
-
             // If there is a valid list of {@link FilmNews}es, then add them to the adapter's
             // data set. This will trigger the ListView to update.
             if (filmNewses != null && !filmNewses.isEmpty()) {
@@ -83,7 +79,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
     public void onLoaderReset(Loader<List<FilmNews>> loader) {
         //Loader reset, so we can clear out our existing data.
         mAdapter.clear();
-        Log.i(LOG_TAG, "onLoadReset");
     }
 
     @Override
@@ -124,18 +119,15 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         if (getNetworkInfo() != null && getNetworkInfo().isConnected()) {
             //Get a reference to the LoaderManager, in order to interact with loaders.
             LoaderManager loaderManager = getLoaderManager();
-
             //Initialize the loader. Pass in the int ID constant defined above and pass in null for
             //the bundle. Pass in this activity for the LoaderCallbacks parameter (which is valid
             //because this activity implements the LoaderCallbacks interface).
             loaderManager.initLoader(FILMNEWS_LOADER_ID, null, this);
-            Log.i(LOG_TAG, "initLoader");
         } else {
             //Otherwise, display error
             //First, hide loading indicator so error message will be visible
             View loadingIndicator = findViewById(R.id.loading_spinner);
             loadingIndicator.setVisibility(View.GONE);
-
             //Update empty state with no connection error message
             mEmptyStateTextView.setText(R.string.no_internet);
         }
@@ -145,7 +137,6 @@ public class MainActivity extends AppCompatActivity implements LoaderCallbacks<L
         //Get a reference to the ConnectivityManager to check state of network connectivity
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
-
         //Return details on the currently active default data network
         return connMgr.getActiveNetworkInfo();
     }
